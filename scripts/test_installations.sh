88c8fc5f652fcc35be0a2da9d953001e712160ee
cicdguy <26552821+cicdguy@users.noreply.github.com>
2024-03-06 09:16:26 -0600

    Update default R version + add test suite + install Pharmaverse pkgs from CRAN (#92)
    - [x] Updates the default R version to R 4.3.3
    - [x] Add a simple test script
    - [x] Update GHA actions
    - [x] Install Pharmaverse packages from CRAN instead of r-universe.dev
    - [x] Support multiplatform builds

---
+++ scripts/test_installations.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+set -eo pipefail
+
+echo "🔎 Checking installations..."
+
+function check_installation() {
+    if command -v "$1" &> /dev/null
+    then
+        echo "✅ $1 is installed."
+    else
+        echo "❌ $1 is not installed."
+        exit 1
+    fi
+}
+
+declare -A bins=(
+    [Python]="python"
+    [Java]="java"
+    [R]="R"
+    [LaTeX]="pdflatex"
+)
+
+for bin in "${!bins[@]}"; do
+    check_installation "${bins[$bin]}"
+done
